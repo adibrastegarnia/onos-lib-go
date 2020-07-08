3730fe1b84dcbdc30018e7bbe7c6d1787a86faea
adibrastegarnia <adib.rastegarnia@gmail.com>
2020-07-08 13:23:17 -0700

    Revert "Support RS type algorithms for authentication"
    This reverts commit aa1e3db54078855e4975a078358b6a9821f8bf39.

--- pkg/auth/jwt.go
+++ pkg/auth/jwt.go
@@ -29,8 +29,6 @@ import (
 const (
 	// SharedSecretKey shared secret key for signing a token
 	SharedSecretKey = "SHARED_SECRET_KEY"
-	// RSAPublicKey an RSA public key
-	RSAPublicKey = "RSA_PUBLIC_KEY"
 )
 
 // JwtAuthenticator jwt authenticator
@@ -44,14 +42,6 @@ func (j *JwtAuthenticator) parseToken(tokenString string) (*jwt.Token, jwt.MapCl
 		if strings.HasPrefix(token.Method.Alg(), "HS") {
 			key := os.Getenv(SharedSecretKey)
 			return []byte(key), nil
-			// RS256, RS384, or RS512
-		} else if strings.HasPrefix(token.Method.Alg(), "RS") {
-			key := os.Getenv(RSAPublicKey)
-			rsaPublicKey, err := jwt.ParseRSAPublicKeyFromPEM([]byte(key))
-			if err != nil {
-				return nil, err
-			}
-			return rsaPublicKey, nil
 		}
 		return nil, fmt.Errorf("unknown signining algorithm: %s", token.Method.Alg())
 	})
@@ -60,8 +50,8 @@ func (j *JwtAuthenticator) parseToken(tokenString string) (*jwt.Token, jwt.MapCl
 
 }
 
-// ParseAndValidate parse a jwt string token and validate it
-func (j *JwtAuthenticator) ParseAndValidate(tokenString string) (jwt.MapClaims, error) {
+// Authenticate parse a jwt string token and authenticate it
+func (j *JwtAuthenticator) Authenticate(tokenString string) (jwt.MapClaims, error) {
 	token, claims, err := j.parseToken(tokenString)
 	if err != nil {
 		return nil, err
